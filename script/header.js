2210a158ad1e3b65fd3a1b22e2a14f8fc8a7a07b
Seohyun Jung <sunny1762@naver.com>
2026-03-14 22:30:54 +0900

    header-script

--- script/header.js
+++ script/header.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
     // 메뉴 버튼을 누르면 header가 나오는 기능
-    const btnMenu =document.querySelector(".btn-menu");
+    const btnMenu = document.querySelector(".btn-menu");
+    const menuSmartHidden = document.querySelector(".menu-smart-hidden")
+    const btnClose = document.querySelector(".btn-close");
+    const gnbSmartList = document.querySelector(".gnb-smart>li")
+
+    btnMenu.addEventListener("click",()=>{
+        menuSmartHidden.classList.add('on');
+    })
+    btnClose.addEventListener("click",()=>{
+        menuSmartHidden.classList.remove(`on`);
+        gnbSmartList.forEach(tag=>tag.classList.remove("on"))
+    })
+
+    // 모바일에서 2뎁스 메뉴 나오는 기능
+    const btnMores = document.querySelectorAll(".gnb-smart>li .btn-more")
+
+    btnMores.forEach(span=>{
+        span.addEventListener(`click`,()=>{
+            span.parentElement.classList.toggle("on")
+        })
+    })
 })
